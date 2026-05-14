e381e054a95742e3cdf25dc77671a33a95484131
Pedro Torres <catomeower@gmail.com>
2026-05-14 20:36:43 +0200

    Versión inicial del programa, .gitignore y README

---
+++ README.md
@@ -0,0 +1,56 @@
+# Presentación técnica
+
+
+## Título: Tests unitarios
+
+### Descripción:
+
+Este software consiste en una serie de tests unitarios realizados sobre un simple programa de
+python que ordena una serie de palabras introducidas.
+
+### Tecnologías utilizadas:
+
+- Python 3.14.2 
+- Unittest
+
+## Guía de despliegue:
+
+Este software no requiere de ningún tipo de instalación, simplemente clonando el repositorio y
+ejecutando el programa main.py se realizan las pruebas incluidas en el código.
+
+### Clonado y ejecución:
+
+```bash
+git clone https://github.com/catodevops/tarea_online_5.git
+cd tarea_online_5
+python main.py
+```
+
+## Tabla de trazabilidad
+
+| Versión | Referencia Commit | Cambios realizados |
+|---------|-------------------|--------------------|
+|v0.1-----|6ef1ce1------------|Inicialización del proyecto|
+|v0.2-----|12aecd9------------|Creación de branch desarrollo-seguro|
+|v1.0-----|-------------------|Implementación del código funcional, README y .gitignore|
+
+## Checklist de seguridad
+
+Este proyecto contiene un archivo .gitignore donde se especifican una serie de carpetas y
+archivos excluidos a la hora de subir cambios, los cuales están relacionados principalmente
+con la higiene del repositorio. En mi caso he decidido ignorar un archivo cambios.txt que se encuentra en mi equipo local, sin ninguna razón técnica aparte de demostrar el correcto funcionamiento de .gitignore. Para el resto de exclusiones, el razonamiento es el siguiente:
+
+
+- `__pycache__/`
+Dado que contiene archivos de caché generados automáticamente por Python que no resultan útiles.
+- `*.pyc`
+Dado que contiene archivos automáticamente compilados de Python.
+- `.vscode/`
+Dado que contiene la configuración local de Visual Studio Code, que es el entorno que he utilizado.
+
+
+Todos estos archivos son excluidos dado que no aportan ningún tipo de funcionalidad al programa:
+- Son generados automáticamente por el entorno de trabajo utilizado
+- Podría causar conflictos con ciertas máquinas
+- Excluirlos hace más legible el contenido del repositorio
+- Se considera mala práctica subir archivos no vitales y potencialmente problemáticos a un repositorio.
--- main.py
+++ main.py
@@ -1,1 +1,84 @@
-"Se han realizado cambios desde que se creó la rama desarrollo-seguro"
+#Se han realizado cambios desde que se creó la rama desarrollo-seguro
+
+import unittest 
+ 
+def cadena_mas_larga(lista_cadenas): 
+  
+    #Comprobación simple de si la lista está vacía  
+    if len(lista_cadenas) == 0: 
+        return "" 
+    #Creamos la lista para almacenar las cadenas más largas 
+    cadenas_mas_largas = [] 
+    longitud_maxima = 0 
+ 
+    #Recorremos la lista 
+    for cadena_actual in lista_cadenas: 
+        #Nos aseguramos de que la cadena sea un string 
+        if isinstance(cadena_actual, str): 
+            #Contamos las letras de la cadena actual 
+            longitud = len(cadena_actual) 
+ 
+            #Guardamos la cadena más larga 
+            if longitud > longitud_maxima: 
+                longitud_maxima = longitud 
+                cadenas_mas_largas = [cadena_actual] 
+            #Añadimos las cadenas con la misma longitud 
+            elif longitud == longitud_maxima: 
+                cadenas_mas_largas.append(cadena_actual) 
+ 
+    #Ordenamos alfabéticamente y devolvemos la primera 
+    cadenas_mas_largas.sort() 
+    #Nos aseguramos de que no esté vacío 
+    if len(cadenas_mas_largas) == 0: 
+        return "" 
+    else: 
+        return cadenas_mas_largas[0] 
+ 
+ 
+#Programa principal para introducir las 5 palabras 
+def introduce_palabras(): 
+    palabras = [] 
+    print("Introduce 5 palabras:") 
+    while len(palabras) < 5: 
+        palabra = input(f"Palabra {len(palabras) + 1}: ") 
+        if " " in palabra: 
+            print("Error: Solo se permite escribir una palabra, no puedes usar espacios.") 
+        else: 
+            palabras.append(palabra) 
+     
+    resultado = cadena_mas_larga(palabras) 
+    print("La palabra más larga es:", resultado) 
+ 
+class Test(unittest.TestCase): 
+ 
+    def test_1_lista_enunciado(self): 
+        lista = ["a", "ab", "abc", "dddd", "abcd"] 
+        print("Lista introducida: ", lista) 
+        resultado = cadena_mas_larga(lista) 
+        print("Lista del enunciado: ", lista, "\nResultado: ", resultado) 
+        self.assertEqual(resultado, "abcd") 
+ 
+    def test_2_misma_longitud(self): 
+        lista = ["palo", "hola", "lata"] 
+        print("Lista introducida: ", lista) 
+        resultado = cadena_mas_larga(lista) 
+        # Todas tienen la misma longitud, debe devolver la primera alfabéticamente 
+        print("Lista con la misma longitud: ",lista, "\nResultado: ", resultado) 
+        self.assertEqual(resultado, "hola") 
+ 
+    def test_3_lista_vacia(self): 
+        lista = [] 
+        print("Lista introducida: ", lista) 
+        resultado = cadena_mas_larga(lista) 
+        print("Se ha introducido una lista vacía: ", lista, "\nResultado: ", resultado) 
+        self.assertEqual(resultado, "") 
+    def test_4_valor_nulo(self): 
+        lista = ["Rojo", None, "Azul"] 
+        print("Lista introducida: ", lista) 
+        resultado = cadena_mas_larga(lista) 
+        print("Lista con un valor nulo: ", lista, "\nResultado: ", resultado) 
+        self.assertEqual(resultado, "Azul") 
+
+#introduce_palabras() 
+#Comenta el programa anterior y descomenta el siguiente para realizar las pruebas 
+unittest.main() 
